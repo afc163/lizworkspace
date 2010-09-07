51b75cffbe32f112a2b7245b02114e136a1947c1
lizzie <shengyan1985@gmail.com>
2010-09-07 09:30:11 +0800

    update new style

--- getstartedwithkissy/docs/demo/starscore.js
+++ getstartedwithkissy/docs/demo/starscore.js
@@ -29,11 +29,16 @@ KISSY.add('starrating', function(S){
             <div class="shop-rating">
                 <span class="title">{{items.title}}</span>
                 <ul class="rating-level">
-                    <li><a class="one-star" data-star-value="{{items.value1}}" href="#">{{items.value1}}</a></li>
-                    <li><a class="two-stars" data-star-value="{{items.value2}}" href="#">{{items.value2}}</a></li>
-                    <li><a class="three-stars" data-star-value="{{items.value3}}" href="#">{{items.value3}}</a></li>
-                    <li><a class="four-stars" data-star-value="{{items.value4}}" href="#">{{items.value4}}</a></li>
-                    <li><a class="five-stars" data-star-value="{{items.value5}}" href="#">{{items.value5}}</a></li>
+                    <li><a class="one-star" data-star-value="{{items.value1}}" href="#">
+                        {{items.value1}}</a></li>
+                    <li><a class="two-stars" data-star-value="{{items.value2}}" href="#">
+                        {{items.value2}}</a></li>
+                    <li><a class="three-stars" data-star-value="{{items.value3}}" href="#">
+                        {{items.value3}}</a></li>
+                    <li><a class="four-stars" data-star-value="{{items.value4}}" href="#">
+                        {{items.value4}}</a></li>
+                    <li><a class="five-stars" data-star-value="{{items.value5}}" href="#">
+                        {{items.value5}}</a></li>
                 </ul>
                 <span class="result"></span>
                 <input type="hidden" name="{{items.name}}" value="" size="2">
@@ -83,9 +88,11 @@ KISSY.add('starrating', function(S){
                 S.each(DOM.query('a', t), function(a, j, arr) {
                     
                     // construct default reason if undefined
-                    if (self.config.reason[i][j] === undefined ) self.config.reason[i][j] = DEFAULT_REASON;
+                    if (self.config.reason[i][j] === undefined ) 
+                        self.config.reason[i][j] = DEFAULT_REASON;
                     // construct default level if undefined
-                    if (self.config.level[j] === undefined ) self.config.level[j] = DEFAULT_LEVEL;
+                    if (self.config.level[j] === undefined ) 
+                        self.config.level[j] = DEFAULT_LEVEL;
                     
                     var sc = DOM.attr(a, MA_RATING_VALUE),
                         rs = self.config.reason[i][j];
@@ -108,8 +115,11 @@ KISSY.add('starrating', function(S){
                             h = DOM.height(this),
                             w = DOM.width(this);
                         // adjust poptips' location & msg
-                        DOM.html(self.poptips, '<span><em>'+sc+'</em> 分 '+self.config.level[j]+'</span><strong>'+rs+'</strong>');
-                        DOM.css(self.poptips, 'left', offset.left+w-coffset.left-100+'px'); // 100 is related with the width of self.poptips
+                        DOM.html(self.poptips, '<span><em>'+sc+'</em> 分 '+ 
+                                                self.config.level[j]+ 
+                                                '</span><strong>'+rs+'</strong>');
+                        
+                        DOM.css(self.poptips, 'left', offset.left+w-coffset.left-100+'px'); 
                         DOM.css(self.poptips, 'top', offset.top-coffset.top+h+'px');
                         DOM.removeClass(self.poptips, HIDE);
                     });
